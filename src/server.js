0a54de10ad12adcf4462297ec47e7928b6db0ddb
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2023-11-30 14:13:12 -0300

    :construction: creation and listing routes (HTTP Methods)

--- src/server.js
+++ src/server.js
@@ -1,7 +1,17 @@
 import http from 'node:http'
 
 const server = http.createServer((request, response) => {
-    return response.end('Hello Wordd!')
+    const { method, url } = request
+
+    if(method === 'GET' && url === '/users') {
+        return response.end('User list')
+    }
+
+    if(method === 'POST' && url === '/users') {
+        return response.end('User creation')
+    }
+
+    return response.end('Hello Word!') 
 })
 
 server.listen(3333)
