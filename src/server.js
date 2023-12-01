9c690095aa7bba2ee828d5f14c4c1b2eb9d40323
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2023-11-30 21:36:17 -0300

    :construction: saving users in memory (Headers)

--- src/server.js
+++ src/server.js
@@ -1,13 +1,23 @@
 import http from 'node:http'
 
+const users = []
+
 const server = http.createServer((request, response) => {
     const { method, url } = request
 
     if(method === 'GET' && url === '/users') {
-        return response.end('User list')
+        return response
+            .setHeader('Content-type', 'aplication/json')
+            .end(JSON.stringify(users))
     }
 
     if(method === 'POST' && url === '/users') {
+        users.push({
+            id: 1,
+            name: 'John Doe',
+            email: 'johndoe@example.com'
+        })
+
         return response.end('User creation')
     }
 
