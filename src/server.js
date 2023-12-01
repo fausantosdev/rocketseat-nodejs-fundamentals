06731d084d46eac062d32dfc9da27430dbc5e19b
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2023-11-30 22:10:53 -0300

    :construction: knowing HTTP status codes / add .editorconfig

--- src/server.js
+++ src/server.js
@@ -3,25 +3,29 @@ import http from 'node:http'
 const users = []
 
 const server = http.createServer((request, response) => {
-    const { method, url } = request
+  const { method, url } = request
 
-    if(method === 'GET' && url === '/users') {
-        return response
-            .setHeader('Content-type', 'aplication/json')
-            .end(JSON.stringify(users))
-    }
+  if(method === 'GET' && url === '/users') {
+      return response
+                    .setHeader('Content-type', 'aplication/json')
+                    .end(JSON.stringify(users))
+  }
 
-    if(method === 'POST' && url === '/users') {
-        users.push({
-            id: 1,
-            name: 'John Doe',
-            email: 'johndoe@example.com'
-        })
+  if(method === 'POST' && url === '/users') {
+    users.push({
+      id: 1,
+      name: 'John Doe',
+      email: 'johndoe@example.com'
+    })
 
-        return response.end('User creation')
-    }
+    return response
+                  .writeHead(201)    
+                  .end()
+}
 
-    return response.end('Hello Word!') 
+  return response
+                .writeHead(404)
+                .end()
 })
 
 server.listen(3333)
